de87d1b5b68fdb984e8840a88778a1e1cd039bfc
Michal Nowak <michal.nowak@softmaker.io>
2023-10-04 21:47:23 +0200

    Implement delete feature

--- client/src/features/posts/PostDetails.jsx
+++ client/src/features/posts/PostDetails.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from "react";
 import { useParams, useNavigate, Link } from "react-router-dom"
+
 import { API_URL } from "../../constants";
 
 function PostDetails() {
     const [post, setPost] = useState(null);
     const { id } = useParams();
+    const navigate = useNavigate();
     useEffect(() => {
         const fetchCurrentPost = async () => {
             try {
@@ -22,13 +24,30 @@ function PostDetails() {
         fetchCurrentPost();
     }, [id]);
 
+    const deletePost = async () => {
+        try {
+            const response = await fetch(`${API_URL}/${id}`, {
+                method: "DELETE",
+            });
+            if (response.ok) {
+                //pconst json = await response.json();
+                navigate("/")
+            } else {
+                throw response;
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     if (!post) return <h2>Loading...</h2>;
     
     return <div>
         <h2>{post.title}</h2>
         <p>{post.body}</p>
         <Link to="/">Back to Posts</Link>
-
+        {" | "}
+        <button onClick={deletePost}>Delete</button>
     </div>;
 
 
