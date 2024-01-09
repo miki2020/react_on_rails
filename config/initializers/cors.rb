b8a14ef714801f7c60d6f60f86e2e5558cab32d7
Michał Nowak <minow1980@gmail.com>
2024-01-09 17:32:17 +0100

    Render deploy cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "https://static-react-client.onrender.com"
     # origins "*"
 
     resource "*",
