36dde834537cecf9339a865ef5229350db067558
Michał Nowak <minow1980@gmail.com>
2024-01-09 14:22:43 +0100

    Change db engine to Postgresql

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -6,37 +6,36 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get posts_url, as: :json
+    get api_v1_posts_url, as: :json
     assert_response :success
   end
 
   test "should return posts in decending order" do
-    get posts_url, as: :json
+    get api_v1_posts_url, as: :json
     assert_equal Post.order(created_at: :desc), assigns(:posts)
-
   end
 
   test "should create post" do
     assert_difference("Post.count") do
-      post posts_url, params: { post: { body: @post.body, title: @post.title } }, as: :json
+      post api_v1_posts_url, params: { post: { body: @post.body, title: @post.title } }, as: :json
     end
 
     assert_response :created
   end
 
   test "should show post" do
-    get post_url(@post), as: :json
+    get api_v1_post_url(@post), as: :json
     assert_response :success
   end
 
   test "should update post" do
-    patch post_url(@post), params: { post: { body: @post.body, title: @post.title } }, as: :json
+    patch api_v1_post_url(@post), params: { post: { body: @post.body, title: @post.title } }, as: :json
     assert_response :success
   end
 
   test "should destroy post" do
     assert_difference("Post.count", -1) do
-      delete post_url(@post), as: :json
+      delete api_v1_post_url(@post), as: :json
     end
 
     assert_response :no_content
